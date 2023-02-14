a7e1b19ff714f4eba9315e92350130de9cac4ab1
nunegabriel <nune.gabriel.gn@gmail.com>
2023-02-14 23:25:23 +0000

    FIX LINTERS

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-        # user_id = params[:user_id]
+    # user_id = params[:user_id]
     # Use the user_id parameter to fetch the relevant posts
     # For example: posts = Post.where(user_id: user_id)
     # Render the posts as a JSON response
