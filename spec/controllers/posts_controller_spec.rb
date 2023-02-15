07caf85a1585284dc52efe7cac5988bb72f29400
nunegabriel <nune.gabriel.gn@gmail.com>
2023-02-15 16:50:50 +0000

    ADD POST CONTROLLER

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+RSpec.describe PostsController, type: :controller do
+  describe "GET #index" do
+    it "returns a successful response" do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #show" do
+    it "returns a successful response" do
+      # Assume we have a post with ID 1 in the database
+      get :show, params: { id: 1 }
+      expect(response).to be_successful
+    end
+
+end
