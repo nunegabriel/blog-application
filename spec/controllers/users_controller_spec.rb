d1680dbe65f960376d58cf7da922742ddc11a510
nunegabriel <nune.gabriel.gn@gmail.com>
2023-02-15 17:19:15 +0000

    FIX LINTERS

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
-  describe "GET #index" do
-    it "returns a successful response" do
+  describe 'GET #index' do
+    it 'returns a successful response' do
       get :index
       expect(response).to be_successful
     end
   end
 
-  describe "GET #show" do
-    it "returns a successful response for an existing user" do
+  describe 'GET #show' do
+    it 'returns a successful response for an existing user' do
       # Create a sample user
       user = FactoryBot.create(:user)
 
@@ -16,12 +16,9 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to be_successful
     end
 
-    it "returns a 404 error for a non-existing user" do
-      get :show, params: { id: 12345 }
+    it 'returns a 404 error for a non-existing user' do
+      get :show, params: { id: 12_345 }
       expect(response.status).to eq(404)
     end
   end
-end
-
-
 end
